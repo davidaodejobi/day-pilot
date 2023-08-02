3c093dbbbab095d47400f303c023ba739230e886
davidaodejobi <talk2engineerdavid@gmail.com>
2023-08-02 03:27:18 +0100

    feat: dashboard ui checkpoin

--- lib/src/features/dashboard/dashboard.dart
+++ lib/src/features/dashboard/dashboard.dart
@@ -85,160 +85,8 @@ class DashBoard extends HookConsumerWidget {
                       )
                     : ListView(
                         padding: EdgeInsets.symmetric(horizontal: 16.w),
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 16.h),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 16.w, vertical: 8.h),
-                            decoration: BoxDecoration(
-                              color: AppColors.scaffoldBackgroundColor,
-                              borderRadius: BorderRadius.circular(10),
-                              //shadow
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.1),
-                                  spreadRadius: 2,
-                                  blurRadius: 3,
-                                  offset: const Offset(0, 2),
-                                ),
-                              ],
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  'Learn DSA (Dynamic Programming) and solve 2 problems',
-                                ),
-                                12.hi,
-                                Wrap(
-                                  spacing: 8.w,
-                                  runSpacing: 8.h,
-                                  children: [
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 8.w, vertical: 4.h),
-                                      decoration: BoxDecoration(
-                                        color: AppColors.primaryColor
-                                            .withOpacity(0.1),
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      child: RichText(
-                                        text: TextSpan(
-                                          text: 'Deadline: ',
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .titleSmall!
-                                              .copyWith(
-                                                fontSize: 12.sp,
-                                                color: AppColors.textColor,
-                                                fontWeight: FontWeight.w400,
-                                              ),
-                                          children: <TextSpan>[
-                                            TextSpan(
-                                              text: '11:59 PM',
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .titleSmall!
-                                                  .copyWith(
-                                                    fontSize: 12.sp,
-                                                    color:
-                                                        AppColors.primaryColor,
-                                                    fontWeight: FontWeight.w600,
-                                                  ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                    Container(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 8.w, vertical: 4.h),
-                                      decoration: BoxDecoration(
-                                        color: AppColors.greyTextColor
-                                            .withOpacity(0.1),
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Text(
-                                            'Prioroty: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall!
-                                                .copyWith(
-                                                  color: AppColors.textColor,
-                                                  fontWeight: FontWeight.w400,
-                                                ),
-                                          ),
-                                          SvgPicture.asset(
-                                            "p-low".svg,
-                                            height: 12.h,
-                                            width: 12.w,
-                                            color: AppColors.primaryColor,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Stack(
-                                      children: [
-                                        Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 8.w, vertical: 4.h),
-                                          decoration: BoxDecoration(
-                                            color: AppColors.greyTextColor
-                                                .withOpacity(0.1),
-                                            borderRadius:
-                                                BorderRadius.circular(5),
-                                          ),
-                                          child: Text(
-                                            '                                   ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall!
-                                                .copyWith(
-                                                  color: AppColors.textColor,
-                                                  fontWeight: FontWeight.w400,
-                                                ),
-                                          ),
-                                        ),
-                                        RichText(
-                                          text: TextSpan(
-                                            text: 'Alloted time: ',
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall!
-                                                .copyWith(
-                                                  fontSize: 12.sp,
-                                                  color: AppColors.textColor,
-                                                  fontWeight: FontWeight.w400,
-                                                ),
-                                            children: <TextSpan>[
-                                              TextSpan(
-                                                text: '2 hours',
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .titleSmall!
-                                                    .copyWith(
-                                                      fontSize: 12.sp,
-                                                      color: AppColors
-                                                          .primaryColor,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                    ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                12.hi,
-                                // Container
-                              ],
-                            ),
-                          ),
+                        children: const [
+                          ColoredStartContainer(),
                         ],
                       ),
               ),
@@ -280,5 +128,204 @@ class DashBoard extends HookConsumerWidget {
         ),
       ),
     );
+  }
+}
+
+class ColoredStartContainer extends StatelessWidget {
+  const ColoredStartContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.h),
+      decoration: BoxDecoration(
+        color: AppColors.primaryColor.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(5.r),
+      ),
+      child: CustomPaint(
+        painter: ColoredStartPainter(), // Custom painter for colored start
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Learn DSA (Dynamic Programming) and solve 2 problems',
+              ),
+              10.hi,
+              Wrap(
+                spacing: 8.w,
+                runSpacing: 6.h,
+                children: [
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Deadline: ',
+                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                              fontSize: 12.sp,
+                              color: AppColors.textColor,
+                              fontWeight: FontWeight.w400,
+                            ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '11:59 PM',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(
+                                  fontSize: 12.sp,
+                                  color: AppColors.primaryColor,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+                    decoration: BoxDecoration(
+                      color: AppColors.greyTextColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Prioroty: ',
+                          style:
+                              Theme.of(context).textTheme.titleSmall!.copyWith(
+                                    color: AppColors.textColor,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                        ),
+                        SvgPicture.asset(
+                          "p-low".svg,
+                          height: 12.h,
+                          width: 12.w,
+                          color: AppColors.highPriorityColor,
+                        ),
+                        4.wi,
+                        const Icon(
+                          Icons.arrow_drop_down,
+                          color: AppColors.textColor,
+                          size: 12,
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Time Allocated: ',
+                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                              fontSize: 12.sp,
+                              color: AppColors.textColor,
+                              fontWeight: FontWeight.w400,
+                            ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '2 hours',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .copyWith(
+                                  fontSize: 12.sp,
+                                  color: AppColors.primaryColor,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+                    decoration: BoxDecoration(
+                      color: AppColors.greyTextColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Status:',
+                          style:
+                              Theme.of(context).textTheme.titleSmall!.copyWith(
+                                    color: AppColors.textColor,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                        ),
+                        Text(
+                          ' In Progress',
+                          style:
+                              Theme.of(context).textTheme.titleSmall!.copyWith(
+                                    color: AppColors.textColor,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                        ),
+                        4.wi,
+                        const Icon(
+                          Icons.arrow_drop_down,
+                          color: AppColors.textColor,
+                          size: 12,
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColoredStartPainter extends CustomPainter {
+  final Color color;
+
+  ColoredStartPainter({
+    this.color = AppColors.primaryColor,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()..color = color; // Color of the start area
+
+    double cornerRadius = 5.0.r; // Radius for rounded corners
+    final Rect rect =
+        Rect.fromLTRB(0, 0, 10.w, size.height); // Start area dimensions
+
+    // Create a Path and add rounded corners to the top left and bottom left
+    final Path path = Path()
+      ..addRRect(
+        RRect.fromRectAndCorners(
+          rect,
+          topLeft: Radius.circular(cornerRadius),
+          bottomLeft: Radius.circular(cornerRadius),
+        ),
+      );
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false; // The painting doesn't change
   }
 }
